f0724c7f8ae2947d2af9edfc5b4217f0964cc439
trevmo303 <trevmo303@192.168.7.216>
2022-01-11 22:31:38 -0700

    Tuesday night... started Card and Players classes

--- 0zc0rp.Monopoly/src/Cards.java
+++ 0zc0rp.Monopoly/src/Cards.java
@@ -1,10 +1,40 @@
 
-public class Cards extends BoardSpace
-	{
+		public class Cards extends BoardSpace
+		{
 
+			
+		private String cardType;
+		private String content;
+		
+		public Cards(String ca, String co)
+		{
+			cardType = ca;
+			content = co;
+		}
+			
+		public String getCardType()
+			{
+				return cardType;
+			}
+
+		public void setCardType(String cardType)
+			{
+				this.cardType = cardType;
+			}
+		
+		public String getContent()
+			{
+				return content;
+			}
+
+		public void setContent(String content)
+			{
+				this.content = content;
+			}
+			
+		}
 	
 		
-		//chance
-		//community chest
+	
 		
-	}
+	
--- 0zc0rp.Monopoly/src/Taxes.java
+++ 0zc0rp.Monopoly/src/Taxes.java
@@ -3,7 +3,7 @@ public class Taxes extends BoardSpace
 		private String propertyTax;
 		private String luxuryTax;
 		
-		public Properties(String p, String l)
+		public void Properties(String p, String l)
 			{
 				propertyTax = p;
 				luxuryTax = l;
