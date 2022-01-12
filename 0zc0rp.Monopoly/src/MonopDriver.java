94639fab6f0dfa4da6235db99da4cf19f03e5680
samuel.ficsor <samuel.ficsor@MU-C1-14.mullen.pvt.k12.co.us>
2022-01-12 10:00:57 -0700

    Fileelelelel

--- 0zc0rp.Monopoly/src/MonopDriver.java
+++ 0zc0rp.Monopoly/src/MonopDriver.java
@@ -24,7 +24,7 @@ public class MonopDriver
 			        	  */
 			        	  if(file.next().equals("Property"))
 			        		  {
-			        			  board[i] = new Properties(file.nextBoolean(), file.nextInt(), file.nextInt(), file.nextInt(), file.nextInt(), file.nextInt(), file.next());
+			        			  board[i] = new Properties(file.next(), file.next(), file.nextInt(), file.next(), file.nextInt(), file.nextBoolean(), file.nextInt(), file.nextInt(), file.nextInt(), file.nextInt(), file.nextInt(), file.next());
 			        		  }
 			          }
 			}
